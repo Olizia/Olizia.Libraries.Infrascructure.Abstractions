a8874333cabc46ebe98403a7be7e6305d5f9bf1b
OtavioVB <otaviovb.developer@gmail.com>
2022-12-03 19:42:45 -0300

    base(archives): `Initial Commit`

---
+++ Olizia.Libraries.Infrascructure.Abstractions/Mappings/BaseMapping.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Olizia.Libraries.Infrascructure.Abstractions.Mappings;
+
+public interface BaseMapping<Entity>
+    where Entity : class
+{
+    public void CreateMapping(EntityTypeBuilder<Entity> entity);
+}
---
+++ Olizia.Libraries.Infrascructure.Abstractions/Repositories/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Olizia.Libraries.Infrascructure.Abstractions.Repositories;
+
+public interface IBaseRepository<TEntity>
+    where TEntity : class
+{
+    Task AddAsync(TEntity entity);
+    Task AddRangeAsync(List<TEntity> entities);
+    Task<TEntity?> GetAsync(Guid identifier);
+    Task<List<TEntity>> GetAllAsync();
+    Task DeleteAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+}
